9370e31198e1c9627197f5dc66b85189d8afd57c
Jonas Nicklas <jonas@jnicklas.com>
2025-10-22 18:31:24 +0200

    Jump to parent key if child key not found

--- src/get-yaml-line-number.ts
+++ src/get-yaml-line-number.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { resolve } from "path";
-import { Node, parseDocument, Scalar, YAMLMap } from "yaml";
+import { Node, parseDocument, Range, Scalar, YAMLMap } from "yaml";
 
 function isMap(node: Node): node is YAMLMap<Node, Node> {
   return Array.isArray((node as any).items);
@@ -10,20 +10,24 @@ function isScalar(node: Node): node is Scalar {
   return node instanceof Scalar;
 }
 
-function getNodeOffset(node: Node, pathParts: string[]) {
+function getNodeOffset(
+  node: Node,
+  pathParts: string[]
+): Range | null | undefined {
   const [key, ...rest] = pathParts;
-
-  console.debug("Searching for key:", key, "in node:", node);
 
   if (isMap(node)) {
     for (const item of node.items) {
-      console.debug("Checking item:", item);
       if (isScalar(item.key) && item.key.value === key) {
-        console.debug("Found matching key:", key, item.key.range);
         if (rest.length === 0) {
           return item.key.range;
         } else if (item.value) {
-          return getNodeOffset(item.value, rest);
+          const result = getNodeOffset(item.value, rest);
+          if (result) {
+            return result;
+          } else {
+            return item.key.range;
+          }
         }
       }
     }
@@ -39,20 +43,15 @@ export function getYamlLineNumber(
   filePath: string,
   keyPath: string
 ): Location | undefined {
-  console.debug("Read file content from", filePath);
   const content = fs.readFileSync(filePath, "utf8");
   const doc = parseDocument(content).contents;
   if (doc) {
-    console.debug("YAML document parsed successfully");
     const offset = getNodeOffset(doc, keyPath.split("."));
-    console.debug(`Computed offset for keyPath "${keyPath}":`, offset);
     if (offset) {
       return {
         path: resolve(filePath),
         line: content.slice(0, offset[0]).split("\n").length,
       };
     }
-  } else {
-    console.debug("parsing failed, not a valid YAML document");
   }
 }
